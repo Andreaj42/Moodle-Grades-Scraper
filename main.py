81fe6f9d0f739db6f27469332b46c577325dea46
joly.andrea <andrea.joly@telecom-st-etienne.fr>
2023-06-11 03:22:49 +0200

    background scheduler

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ __author__ = "Andréa Joly"
 __date__ = "31-01-2023"
 
 from lib.check import do_check
-from apscheduler.schedulers.blocking import BackgroundScheduler 
+from apscheduler.schedulers.blocking import BlockingScheduler 
+
 
 if __name__ == "__main__":
-    scheduler = BackgroundScheduler(timezone='Europe/Paris')
-    scheduler.add_job(do_check, "interval", seconds=300)
+    scheduler = BlockingScheduler(timezone='Europe/Paris')
+    scheduler.add_job(do_check, "interval", seconds=30)
     scheduler.start()
